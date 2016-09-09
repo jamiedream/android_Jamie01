45d064b2d18208ea3629c8e47b8410e0f8a44953
jamiedream <jamie78831@gmail.com>
2016-09-09 10:46:36 +0800

    v1

---
+++ app/src/main/java/jamie01/iii/org/tw/jamie1501/MainActivity.java
@@ -0,0 +1,13 @@
+package jamie01.iii.org.tw.jamie1501;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
