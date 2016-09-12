a9c82697c39997465c2a48ebc6b7e62285b79060
jamiedream <jamie78831@gmail.com>
2016-09-12 16:36:28 +0800

    v3

--- app/src/main/java/android_jamie/listtest1/MainActivity.java
+++ app/src/main/java/android_jamie/listtest1/MainActivity.java
@@ -2,6 +2,8 @@ package android_jamie.listtest1;
 // List View
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -10,39 +12,39 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView list;
+    private LinkedList<HashMap<String,Object>> data;
+    private String[] from = {"title", "content", "img"};
+    private int[] to = {R.id.item_title, R.id.item_content, R.id.item_img};
+    private EditText inputTitle;
+    private SimpleAdapter adapter;
+    private int[] c = {R.drawable.coffee1, R.drawable.coffee2, R.drawable.coffee3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        inputTitle = (EditText)findViewById(R.id.inputTitle);
         list = (ListView)findViewById(R.id.list);
         initList();
 
     }
     private void initList() {
-        LinkedList<HashMap<String,String>> data = new LinkedList<>();
-        HashMap<String,String> data0 = new HashMap<>();
-        data0.put("title","Jamie");
-        data0.put("content","Jamie...1");
-        data.add(data0);
-        HashMap<String,String> data1 = new HashMap<>();
-        data1.put("title","Judy");
-        data1.put("content","Jamie...2");
-        data.add(data1);
-        HashMap<String,String> data2 = new HashMap<>();
-        data2.put("title","Jimmy");
-        data2.put("content","Jamie...3");
-        data.add(data2);
+        data = new LinkedList<>();
 
-
-        String[] from = {"title", "content"};
-        int[] to = {R.id.item_title, R.id.item_content};
-        SimpleAdapter adapter =
-                new SimpleAdapter(
+        adapter =new SimpleAdapter(
                         this,data,
                         R.layout.layout_item,
                         from,to);
         list.setAdapter(adapter);
     }
-
+    public void addItem(View v){
+        String input = inputTitle.getText().toString();
+        HashMap<String, Object> dd = new HashMap<>();
+        dd.put(from[0], input);
+        dd.put(from[1], "data....");
+        dd.put(from[2], c[(int)(Math.random()*3)]);
+        data.add(dd);
+        adapter.notifyDataSetChanged();
+        //將change傳給adapter
+    }
 }
