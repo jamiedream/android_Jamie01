8d6300da5d2128fbc8de0082120f5a232b20ab88
jamiedream <jamie78831@gmail.com>
2016-09-19 16:50:41 +0800

    v2

--- app/src/main/java/android_jamie/networktest/MainActivity.java
+++ app/src/main/java/android_jamie/networktest/MainActivity.java
@@ -2,10 +2,15 @@ package android_jamie.networktest;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -21,10 +26,16 @@ import java.util.Enumeration;
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager mgr;
     private String data;
+    private TextView mesg;
+    private StringBuffer sb;
+    private UIHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        mesg = (TextView)findViewById(R.id.mesg);
 
         mgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo info = mgr.getActiveNetworkInfo();
@@ -40,36 +51,84 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.d("jamie",e.toString());
             }
             Log.d("jamie", "Conneted");
         }else {
             Log.d("jamie", "Not Conneted");
         }
     }
-    public void test(View v){
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://data.coa.gov.tw/Service/OpenData/EzgoTravelFoodStay.aspx");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.connect();
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    data = reader.readLine();
-                    reader.close();
+    public void test(View v) {
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                try {
+//                    URL url = new URL("http://data.coa.gov.tw/Service/OpenData/EzgoTravelFoodStay.aspx");
+//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                    conn.connect();
+//                    BufferedReader reader =
+//                            new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//                    data = reader.readLine();
+//                    reader.close();
+//                    parseJSON();
+//                } catch (Exception e) {
+//                    Log.d("jamie", e.toString());
+//                }
+//            }
+//        }.start();
+        mesg.setText("");
 
-                } catch (Exception e) {
-                    Log.d("jamie", e.toString());
-                }
+        MyTread mt1 = new MyTread();
+        mt1.start();
+    }
+
+    private class MyTread extends Thread {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://m.coa.gov.tw/OpenData/FarmTransData.aspx");
+                HttpURLConnection conn =  (HttpURLConnection) url.openConnection();
+                conn.connect();
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                data = reader.readLine();
+                reader.close();
+                parseJSON();
+            }catch(Exception ee){
+                Log.d("brad", ee.toString());
             }
-        }.start();
+        }
+    }
 
+
+    private void parseJSON(){
+        sb = new StringBuffer();
+        try {
+            JSONArray root = new JSONArray(data);
+            for (int i=0; i<root.length(); i++){
+                JSONObject row = root.getJSONObject(i);
+                String name = row.getString("交易日期");
+                String addr = row.getString("作物名稱");
+                Log.d("brad", name + " -> " + addr);
+                sb.append(name + " -> " + addr + "\n");
+            }
+            handler.sendEmptyMessage(0);
+        }catch(Exception ee){
+            Log.d("brad", ee.toString());
+        }
+    }
+    public void test2(View v){
 
     }
-    private void parseJSON(View v){
+    private class UIHandler extends android.os.Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mesg.setText(sb);
 
+
+        }
     }
+
 }
 
