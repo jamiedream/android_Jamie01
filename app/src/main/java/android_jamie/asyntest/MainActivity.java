a2adaf041b0d281ae16b06d67dfaceab3925ee62
jamiedream <jamie78831@gmail.com>
2016-09-20 09:12:05 +0800

    v1

---
+++ app/src/main/java/android_jamie/asyntest/MainActivity.java
@@ -0,0 +1,16 @@
+package android_jamie.asyntest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void test(){
+
+    }
+}
