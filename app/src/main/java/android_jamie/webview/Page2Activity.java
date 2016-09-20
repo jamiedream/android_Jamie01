fe9c487d131a1f8f6bf3353937172441ab537e3f
jamiedream <jamie78831@gmail.com>
2016-09-20 16:20:31 +0800

    v3

--- app/src/main/java/android_jamie/webview/Page2Activity.java
+++ app/src/main/java/android_jamie/webview/Page2Activity.java
@@ -1,20 +1,68 @@
 package android_jamie.webview;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Page2Activity extends AppCompatActivity {
     private WebView wv;
+    private LocationManager mgr;
+    private MyGPSListener listener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
-        wv = (WebView)findViewById(R.id.page2_wv);
+        wv = (WebView) findViewById(R.id.page2_wv);
         init();
+        listener = new MyGPSListener();
+        mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mgr.removeUpdates(listener);
+    }
+
     private void init(){
         WebViewClient client = new WebViewClient();
         wv.setWebViewClient(client);
@@ -22,5 +70,38 @@ public class Page2Activity extends AppCompatActivity {
         WebSettings settings = wv.getSettings();
         settings.setJavaScriptEnabled(true);
         wv.loadUrl("file:///android_asset/page2.html");
+        wv.addJavascriptInterface(new MyJS(),"LUCY");
+    }
+    private class MyJS{
+        @JavascriptInterface
+        public void getLatLng(String lat, String lng){
+            Toast.makeText(Page2Activity.this, lat+":"+lng,Toast.LENGTH_SHORT).show();
+        }
+    }
+    private class MyGPSListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getAltitude();
+            double lng = location.getLongitude();
+            wv.loadUrl("javascript:moveMap("+lat+", "+lng+")");
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+    public void test1(View v){
+        wv.loadUrl("javascript:moveMap(0, 0)");
     }
 }
