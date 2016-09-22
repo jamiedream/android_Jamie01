0c45f4970fadc13859bb28dec8afd5e6c0c8ef39
jamiedream <jamie78831@gmail.com>
2016-09-22 14:01:05 +0800

    v1

---
+++ app/src/main/java/android_jamie/mycamera/MainActivity.java
@@ -0,0 +1,16 @@
+package android_jamie.mycamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.img);
+    }
+}
