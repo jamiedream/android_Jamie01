a43c5a09897c24b9c75c9b10c2ae082f0ff4f306
jamiedream <jamie78831@gmail.com>
2016-09-22 12:50:55 +0800

    v2

--- app/src/main/java/android_jamie/mylistv2/List2Activity.java
+++ app/src/main/java/android_jamie/mylistv2/List2Activity.java
@@ -2,22 +2,105 @@ package android_jamie.mylistv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 
+import io.github.codefalling.recyclerviewswipedismiss.SwipeDismissRecyclerViewTouchListener;
+
 public class List2Activity extends AppCompatActivity {
     private RecyclerView recycler;
-    private String[] item = {"item1","item2","item3","item4","item5",};
+    private LinearLayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
+
+    private String[] item = {"item1","item2","item3","item4","item5"};
     private LinkedList<String> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list2);
         data = new LinkedList<>();
-        for(String s:data){
+        for(String s:item){
             data.add(s);
         }
         recycler = (RecyclerView)findViewById(R.id.recyclerView);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter();
+        recycler.setAdapter(mAdapter);
+
+        SwipeDismissRecyclerViewTouchListener listener = new SwipeDismissRecyclerViewTouchListener.Builder(
+                recycler,
+                new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
+                    @Override
+                    public boolean canDismiss(int position) {
+                        return true;
+                    }
+
+                    @Override
+                    public void onDismiss(View view) {
+                        // Do what you want when dismiss
+                        int p = recycler.getChildPosition(view);
+                        data.remove(p);
+                        mAdapter.notifyDataSetChanged();
+
+                    }
+                })
+                .setIsVertical(false)
+                .setItemTouchCallback(
+                        new SwipeDismissRecyclerViewTouchListener.OnItemTouchCallBack() {
+                            @Override
+                            public void onTouch(int index) {
+                                // Do what you want when item be touched
+                            }
+                        })
+                .setItemClickCallback(new SwipeDismissRecyclerViewTouchListener.OnItemClickCallBack() {
+                    @Override
+                    public void onClick(int position) {
+                        // Do what you want when item be clicked
+                    }})
+//                            .setBackgroundId(R.drawable.android, R.drawable.android)
+                    .create();
+                    recycler.setOnTouchListener(listener);
+
+
+
+    }
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+        //內部資料不用傳遞,因此無須建構式
+        class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.itemv2_title);
+            }
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(List2Activity.this)
+                    .inflate(R.layout.layout_item2, parent, false);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mTextView.setText(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
     }
 }
